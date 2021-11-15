bf0846ab17d7bee06370a587da57512f9729bb81
Andrew <shinjo1233@gmail.com>
2021-11-15 15:23:21 -0800

    added grid layout to start working on displaying list of existing notes:

--- frontend/src/components/MainPage.tsx
+++ frontend/src/components/MainPage.tsx
@@ -12,6 +12,20 @@ const MainPage = () => {
             <div className="new-note-panel">
                 <NewNote/>
             </div>
+            <div className="existing-notes-panel">
+                <div className="existing-note">1</div>
+                <div className="existing-note">2</div>
+                <div className="existing-note">3</div>
+                <div className="existing-note">4</div>
+                <div className="existing-note">5</div>
+                <div className="existing-note">6</div>
+                <div className="existing-note">7</div>
+                <div className="existing-note">8</div>
+                <div className="existing-note">9</div>
+                <div className="existing-note">10</div>
+                <div className="existing-note">11</div>
+                <div className="existing-note">12</div>
+            </div>
         </React.Fragment>
     );
 };
